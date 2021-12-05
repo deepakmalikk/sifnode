56baea8f3cf538ee04a833f2b064b83c665c813c
Jure Žvikart <jure@sifchain.finance>
2021-12-05 02:34:30 +0100

    EXP

--- smart-contracts/scripts/do_abigen.sh
+++ smart-contracts/scripts/do_abigen.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+echo "scripts/do_abigen.sh: $1 $2"
+
 jsonfile=$1
 shift
 outputbase=$1
@@ -12,5 +14,6 @@ pkg=$(basename "$jsonfile" .json)
 outputdir=${outputbase}/$jsondir
 mkdir -p $outputdir
 # jq .abi < artifacts/contracts/BridgeBank/BridgeBank.sol/BridgeBank.json | abigen --abi - --pkg Foo
+echo "jq .abi < $jsonfile | abigen --abi - --pkg $pkg --type $pkg --out ${outputdir}/$pkg.go"
 jq .abi < $jsonfile | abigen --abi - --pkg $pkg --type $pkg --out ${outputdir}/$pkg.go
 jq .abi < $jsonfile | abigen --abi - --pkg $pkg --type $pkg --out ${jsondir}/$pkg.go
