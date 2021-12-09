ccb13b2d34d837c6007158ea459a1e559917fc1f
Caner Candan <caner@candan.fr>
2021-12-09 20:41:06 +0100

    test: 💍 add more test coverage for additional funcs

--- x/margin/keeper/keeper_test.go
+++ x/margin/keeper/keeper_test.go
@@ -5,8 +5,10 @@ import (
 
 	"github.com/stretchr/testify/assert"
 
+	clptest "github.com/Sifchain/sifnode/x/clp/test"
 	"github.com/Sifchain/sifnode/x/margin/test"
 	"github.com/Sifchain/sifnode/x/margin/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 func TestKeeper_Errors(t *testing.T) {
@@ -64,3 +66,52 @@ func TestKeeper_DestroyMTP(t *testing.T) {
 	marginKeeper.DestroyMTP(ctx, "xxx", "xxx")
 }
 
+func TestKeeper_ClpKeeper(t *testing.T) {
+	_, app := test.CreateTestAppMargin(false)
+	marginKeeper := app.MarginKeeper
+	assert.NotNil(t, marginKeeper)
+	marginKeeper.ClpKeeper()
+}
+
+func TestKeeper_BankKeeper(t *testing.T) {
+	_, app := test.CreateTestAppMargin(false)
+	marginKeeper := app.MarginKeeper
+	assert.NotNil(t, marginKeeper)
+	marginKeeper.BankKeeper()
+}
+
+func TestKeeper_GetLeverageParam(t *testing.T) {
+	ctx, app := test.CreateTestAppMargin(false)
+	marginKeeper := app.MarginKeeper
+	assert.NotNil(t, marginKeeper)
+	marginKeeper.GetLeverageParam(ctx)
+}
+
+func TestKeeper_CustodySwap(t *testing.T) {
+	pool := clptest.GenerateRandomPool(1)[0]
+	ctx, app := test.CreateTestAppMargin(false)
+	marginKeeper := app.MarginKeeper
+	assert.NotNil(t, marginKeeper)
+	swapResult, err := marginKeeper.CustodySwap(ctx, pool, "xxx", sdk.NewUint(10000))
+	assert.NotNil(t, swapResult)
+	assert.Nil(t, err)
+}
+
+func TestKeeper_Borrow(t *testing.T) {
+	pool := clptest.GenerateRandomPool(1)[0]
+	ctx, app := test.CreateTestAppMargin(false)
+	marginKeeper := app.MarginKeeper
+	assert.NotNil(t, marginKeeper)
+	mtp := types.NewMTP()
+	err := marginKeeper.Borrow(ctx, "xxx", sdk.NewUint(10000), sdk.NewUint(1000), mtp, pool, sdk.NewUint(1))
+	assert.Nil(t, err)
+}
+
+func TestKeeper_UpdatePoolHealth(t *testing.T) {
+	pool := clptest.GenerateRandomPool(1)[0]
+	ctx, app := test.CreateTestAppMargin(false)
+	marginKeeper := app.MarginKeeper
+	assert.NotNil(t, marginKeeper)
+	err := marginKeeper.UpdatePoolHealth(ctx, pool)
+	assert.Nil(t, err)
+}
